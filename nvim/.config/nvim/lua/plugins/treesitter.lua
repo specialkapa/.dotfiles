073203c880c7de6306cfb9e8060448a11bfbf850
Konstantinos Papazafeiropoulos <kostas.papazafeiropoulos@tnp.eu>
2026-01-19 17:52:27 +0000

    fix: fixing nvim treesitter!

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -21,11 +21,18 @@ return { -- Highlight, edit, and navigate code
         }
       end,
     },
+    -- nvim-dap-repl-highlights is set up in the main config function below
+    'LiadOz/nvim-dap-repl-highlights',
   },
-  main = 'nvim-treesitter.config', -- Sets main module to use for opts
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-  opts = {
-    ensure_installed = {
+  config = function()
+    -- IMPORTANT: nvim-dap-repl-highlights.setup() MUST be called BEFORE treesitter setup
+    require('nvim-dap-repl-highlights').setup()
+
+    -- nvim-treesitter main branch setup
+    require('nvim-treesitter').setup {}
+
+    -- Install parsers (main branch API)
+    local parsers_to_install = {
       'lua',
       'python',
       'javascript',
@@ -53,18 +60,30 @@ return { -- Highlight, edit, and navigate code
       'css',
       'html',
       'dap_repl',
-    },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
-  },
+    }
+
+    -- Auto-install missing parsers on startup
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'VeryLazy',
+      callback = function()
+        local installed = require('nvim-treesitter.config').get_installed 'parsers'
+        local installed_set = {}
+        for _, p in ipairs(installed) do
+          installed_set[p] = true
+        end
+        local to_install = {}
+        for _, p in ipairs(parsers_to_install) do
+          if not installed_set[p] then
+            table.insert(to_install, p)
+          end
+        end
+        if #to_install > 0 then
+          require('nvim-treesitter').install(to_install)
+        end
+      end,
+      once = true,
+    })
+  end,
   -- There are additional nvim-treesitter modules that you can use to interact
   -- with nvim-treesitter. You should go explore a few and see what interests you:
   --
