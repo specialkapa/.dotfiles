97946b9a5d87afe8801513ebf99e2ffa3dd2157f
Konstantinos Papazafeiropoulos <kostas.papazafeiropoulos@tnp.eu>
2026-01-23 16:12:36 +0000

    feat: adding multicursor functionality

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -156,4 +156,99 @@ return {
       end, {})
     end,
   },
+  {
+    'jake-stewart/multicursor.nvim',
+    branch = '1.0',
+    config = function()
+      local mc = require 'multicursor-nvim'
+      mc.setup()
+
+      local set = vim.keymap.set
+
+      -- Add or skip cursor above/below the main cursor (vim-style j/k).
+      set({ 'n', 'x' }, '<leader>mk', function()
+        mc.lineAddCursor(-1)
+      end, { desc = 'Add cursor above' })
+      set({ 'n', 'x' }, '<leader>mj', function()
+        mc.lineAddCursor(1)
+      end, { desc = 'Add cursor below' })
+      set({ 'n', 'x' }, '<leader>mK', function()
+        mc.lineSkipCursor(-1)
+      end, { desc = 'Skip cursor above' })
+      set({ 'n', 'x' }, '<leader>mJ', function()
+        mc.lineSkipCursor(1)
+      end, { desc = 'Skip cursor below' })
+
+      -- Add or skip adding a new cursor by matching word/selection
+      set({ 'n', 'x' }, '<leader>mn', function()
+        mc.matchAddCursor(1)
+      end, { desc = 'Add cursor at next match' })
+      set({ 'n', 'x' }, '<leader>ms', function()
+        mc.matchSkipCursor(1)
+      end, { desc = 'Skip next match' })
+      set({ 'n', 'x' }, '<leader>mN', function()
+        mc.matchAddCursor(-1)
+      end, { desc = 'Add cursor at prev match' })
+      set({ 'n', 'x' }, '<leader>mS', function()
+        mc.matchSkipCursor(-1)
+      end, { desc = 'Skip prev match' })
+
+      -- Add and remove cursors with control + left click.
+      set('n', '<c-leftmouse>', mc.handleMouse)
+      set('n', '<c-leftdrag>', mc.handleMouseDrag)
+      set('n', '<c-leftrelease>', mc.handleMouseRelease)
+
+      -- Toggle cursor on/off.
+      set({ 'n', 'x' }, '<leader>mt', mc.toggleCursor, { desc = 'Toggle cursor' })
+
+      -- Mappings defined in a keymap layer only apply when there are
+      -- multiple cursors. This lets you have overlapping mappings.
+      mc.addKeymapLayer(function(layerSet)
+        -- Select a different cursor as the main one (vim-style h/l).
+        layerSet({ 'n', 'x' }, '<leader>mh', mc.prevCursor)
+        layerSet({ 'n', 'x' }, '<leader>ml', mc.nextCursor)
+
+        -- Delete the main cursor.
+        layerSet({ 'n', 'x' }, '<leader>mx', mc.deleteCursor)
+
+        -- Enable and clear cursors using escape.
+        layerSet('n', '<esc>', function()
+          if not mc.cursorsEnabled() then
+            mc.enableCursors()
+          else
+            mc.clearCursors()
+          end
+        end)
+      end)
+
+      -- Customize how cursors look.
+      local hl = vim.api.nvim_set_hl
+      hl(0, 'MultiCursorCursor', { reverse = true })
+      hl(0, 'MultiCursorVisual', { link = 'Visual' })
+      hl(0, 'MultiCursorSign', { link = 'SignColumn' })
+      hl(0, 'MultiCursorMatchPreview', { link = 'Search' })
+      hl(0, 'MultiCursorDisabledCursor', { reverse = true })
+      hl(0, 'MultiCursorDisabledVisual', { link = 'Visual' })
+      hl(0, 'MultiCursorDisabledSign', { link = 'SignColumn' })
+    end,
+  },
+  {
+    'zhisme/copy_with_context.nvim',
+    config = function()
+      require('copy_with_context').setup {
+        -- Customize mappings
+        mappings = {
+          relative = '<leader>yc',
+          absolute = '<leader>Yc',
+          remote = '<leader>cr',
+        },
+        formats = {
+          default = '# {filepath}:{line}', -- Used by relative and absolute mappings
+          remote = '# {remote_url}',
+        },
+        -- whether to trim lines or not
+        trim_lines = false,
+      }
+    end,
+  },
 }
